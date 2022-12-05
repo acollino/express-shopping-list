000a9a56052cd439bea7b174d3305eeba10e973e
Alex Collino <arcollino@gmail.com>
2022-12-05 15:24:22 -0800

    add initial express app setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const routes = require("./src/routes");
+
+const app = express();
+
+// Prepends /api to our routes
+app.use("/api", routes);
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Unspecified error";
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
